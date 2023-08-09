97473fa4d055c7fc48fd48c3ec3da16836bdaac7
Sarah.Keller <sarah29.84@gmail.com>
2023-08-10 00:32:03 +0200

    Integration tests for F1.1 and F1.3 added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import { useState, useEffect } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import { extractLocations, getEvents } from './api';
 import './App.css';
 
 function App() {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState('See all cities');
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents =
+      currentCity === 'See all cities'
+        ? allEvents
+        : allEvents.filter((event) => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, currentNOE)); //creates a new array from index 0 and limits the amount in currentNOE (32)
+    setAllLocations(extractLocations(allEvents));
+  };
+
+  useEffect(() => {
+    //populates list as soon as app component is mounted
+    fetchData();
+  }, [currentCity]);
+
   return (
     <div className="App">
-      <CitySearch />
-      <EventList />
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
       <NumberOfEvents />
+      <EventList events={events} />
     </div>
   );
 }
