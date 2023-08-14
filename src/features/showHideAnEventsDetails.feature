4ebed2717bea3514ec87cf5d272632a2601cd44d
Sarah.Keller <sarah29.84@gmail.com>
2023-08-15 01:07:15 +0200

    Added to show/hide .feature

--- src/features/showHideAnEventsDetails.feature
+++ src/features/showHideAnEventsDetails.feature
@@ -1,16 +1,16 @@
 Feature: Show/Hide Event Details
 
   Scenario: An event element is collapsed by default.
-    Given a list of events is displayed
+    Given user has opened the app
     And the details button is present on an event
-    Then the event element is collapsed by default
+    Then the event details are collapsed by default
 
   Scenario: User can expand an event to see details.
-    Given a list of events is displayed
+    Given user has opened the app
     When the user clicks on the "Show details" button of an event
     Then the event details are visible
 
   Scenario: User can collapse an event to hide details.
-    Given a list of events is displayed
+    Given an event is expanded with visible details
     When the user clicks on the "Hide details" button of the event
     Then the event details are hidden
--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,30 +1,51 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import { render, within, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Event from '../components/Event';
 import App from '../App';
 import { getEvents } from '../api';
 
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature'); //requires full path
 
 defineFeature(feature, (test) => {
-  test('An event element is collapsed by default.', ({ given, and, then }) => {
-    let EventComponent;
-    given('a list of events is displayed', async () => {
-      let AppComponent = render(<App />);
-      const AppDOM = AppComponent.container.firstChild;
-      const EventListDOM = AppDOM.querySelector('#event-list');
+  test('An event element is collapsed by default.', ({ given, when, then }) => {
+    let AppComponent;
+    given('user has opened the app', async () => {
+      AppComponent = render(<App />);
+    });
+    let AppDOM;
+    let EventListDOM;
+    when('the event list is present', async () => {
+      AppDOM = AppComponent.container.firstChild;
+      EventListDOM = AppDOM.querySelector('#event-list');
 
       await waitFor(() => {
         const EventListItems = within(EventListDOM).queryAllByRole('listitem');
         expect(EventListItems.length).toBe(32);
       });
     });
-    and('the details button is present on an event', () => {});
-    then('the event element is collapsed by default', () => {});
+    then('the event details are collapsed by default', async () => {
+      await waitFor(() => {
+        const EventComponent = render(<Event event={event} />);
+        const eventDetails = EventComponent.queryByText(event.description);
+
+        expect(eventDetails).not.toBeInTheDocument();
+      });
+    });
   });
   test('User can expand an event to see details.', ({ given, when, then }) => {
-    given('a list of events is displayed', () => {});
+    let AppComponent;
+    let AppDOM;
+    let EventListDOM;
+    given('user has opened the app', async () => {
+      AppComponent = render(<App />);
+      AppDOM = AppComponent.container.firstChild;
+      EventListDOM = AppDOM.querySelector('#event-list');
+
+      await waitFor(() => {
+        const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+        expect(EventListItems.length).toBe(32);
+      });
+    });
 
     when('the user clicks on the "Show details" button of an event', () => {});
 
