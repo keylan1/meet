085d4261855eca5bc9d90763060ac9ab532ce674
Sarah.Keller <sarah29.84@gmail.com>
2023-08-21 17:27:16 +0200

    Adjusted scatterchart left margin

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -35,7 +35,7 @@ const CityEventsChart = ({ events, allLocations }) => {
           top: 20,
           right: 20,
           bottom: 60,
-          left: 20,
+          left: -30,
         }}>
         <CartesianGrid />
         <XAxis
